8c36e62cb7299832e8e8ce27da59bea3afa375b1
Srikanth Bharadwaj <srikanthbharadwaj@Srikanths-MacBook-Pro.local>
2018-12-30 08:47:03 +0530

    apply linear regression model on the data apply PCA

--- code.py
+++ code.py
@@ -1,8 +1,24 @@
 import pandas as pd
+import numpy as np
 from sklearn.utils import shuffle
 from sklearn import preprocessing
 import sklearn.neural_network as nn
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
 
+def get_all_data():
+    train_values = train_df.values
+    test_values = test_df.values
+    np.random.shuffle(train_values)
+    np.random.shuffle(test_values)
+    X_train = train_values[:, :-1]
+    X_test = test_values[:, :-1]
+    y_train = train_values[:, -1]
+    y_test = test_values[:, -1]
+    return X_train, X_test, y_train, y_test
+
+
+# Read and preprocess data
 test = pd.read_csv("data/test.csv")
 train = pd.read_csv("data/train.csv")
 
@@ -18,11 +34,12 @@ testLabel = test.Activity.values
 encoder = preprocessing.LabelEncoder()
 
 encoder.fit(trainLabel)
-trainLabelE = encoder.transform(trainLabel)
+trainLabelE = encoder.transform(trainLabel)  # Encode labels
 
 encoder.fit(testLabel)
 testLabelE = encoder.transform(testLabel)
 
+# Applying supervised neural network
 mlpSGD = nn.MLPClassifier(hidden_layer_sizes=(90,),
                           max_iter=1000, alpha=1e-4,
                           solver='sgd', verbose=10,
@@ -50,19 +67,19 @@ nnModelLBFGS = mlpLBFGS.fit(trainData, trainLabelE)
 train_df = train.drop("subject", axis=1)
 test_df = test.drop("subject", axis=1)
 
-def get_all_data():
-    train_values = train_df.values
-    test_values = test_df.values
-    np.random.shuffle(train_values)
-    np.random.shuffle(test_values)
-    X_train = train_values[:, :-1]
-    X_test = test_values[:, :-1]
-    y_train = train_values[:, -1]
-    y_test = test_values[:, -1]
-    return X_train, X_test, y_train, y_test
+# Applying linear regression model
+X_train, X_test, y_train, y_test = get_all_data()
+model = LogisticRegression(C=10)
+model.fit(X_train, y_train)
+score = model.score(X_test, y_test)
+print(score)
 
-
-
-
-
-
+# Applying PCA
+X_train, X_test, y_train, y_test = get_all_data()
+pca = PCA(n_components=200)
+pca.fit(X_train)
+X_train = pca.transform(X_train)
+X_test = pca.transform(X_test)
+model.fit(X_train, y_train)
+score = model.score(X_test, y_test)
+print(score)
